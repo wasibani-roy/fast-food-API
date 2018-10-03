8ab42e4fac67dd683ea9590964712fede9f64653
wasibani-roy <40163337+wasibani-roy@users.noreply.github.com>
2018-10-03 21:06:26 +0300

    Update views.py

--- fast_food_api/views.py
+++ fast_food_api/views.py
@@ -2,9 +2,9 @@
 Routes module to handle request urls
 """
 # from fast_food_api.models import resources
-from fast_food_api.models.menu import menu
-from fast_food_api.models.users import user_login, user_registration
-from fast_food_api.models.orders import order_modal, specific_order
+from fast_food_api.controllers.menu import menu_items
+from fast_food_api.controllers.users import user_login, user_registration
+from fast_food_api.controllers.orders import order_modal, specific_order, get_user_history
 
 
 class Urls:
@@ -19,13 +19,13 @@ class Urls:
         :param app:
         :return:
         """
-        app.add_url_rule('/api/v2/register/', view_func=user_registration.as_view('register'),
+        app.add_url_rule('/api/v2/signup/', view_func=user_registration.as_view('register'),
                          methods=['POST'], strict_slashes=False)
         app.add_url_rule('/api/v2/login/', view_func=user_login.as_view('login'),
                          methods=['post'], strict_slashes=False)
-        app.add_url_rule('/api/v2/menu/', view_func=menu.as_view('add_menu'),
+        app.add_url_rule('/api/v2/menu/', view_func=menu_items.as_view('add_menu'),
                          methods=['post'], strict_slashes=False)
-        app.add_url_rule('/api/v2/menu/', view_func=menu.as_view('list_menu_items'),
+        app.add_url_rule('/api/v2/menu/', view_func=menu_items.as_view('list_menu_items'),
                          methods=['get'], strict_slashes=False)
         app.add_url_rule('/api/v2/order/', view_func=order_modal.as_view('place_your_order'),
                          methods=['post'], strict_slashes=False)
@@ -33,3 +33,7 @@ class Urls:
                          methods=['get'], strict_slashes=False)
         app.add_url_rule('/api/v2/order/<int:order_id>/', view_func=specific_order.as_view('get all orders by id'),
                          methods=['get'], strict_slashes=False)
+        app.add_url_rule('/api/v2/user/order/', view_func=get_user_history.as_view('get user history data'),
+                         methods=['get'], strict_slashes=False)
+        app.add_url_rule('/api/v2/order/', view_func=order_modal.as_view('update order details'),
+                         methods=['put'], strict_slashes=False)
